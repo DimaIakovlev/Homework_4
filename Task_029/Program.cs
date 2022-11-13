68250ed279432c69ef54f82ed30fd0afcd5e1817
Yakovlev Dima <zif18@rambler.ru>
2022-11-13 14:43:56 +0500

    add Task_029

---
+++ Task_029/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.Write("Введите число элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = GetRandomArray(size);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int [] GetRandomArray(int size)
+{
+    int [] result = new int [size];
+       for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 100);
+    }
+    return result;
+}
+
